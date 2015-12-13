884a4dd58b1f5073f502251059ea0f0f89db807f
Daniel Neel <dneelyep@gmail.com>
2015-12-12 19:08:00 -0500

    Add comments/switch from sh to bash in the start-rails-server script.

---
+++ scripts/start-rails-server.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Make sure we're in the application directory - otherwise
+# rails server doesn't work.
+cd /home/daniel/Desktop/CategoryCompare-WebApp
+
+# Start the rails server, which hosts the web app.
+rails server
