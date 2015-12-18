42271654c0ec9ab184f13a4269e5298b06e81ec2
Daniel Neel <dneelyep@gmail.com>
2015-12-18 01:46:23 -0500

    Move the logic for starting rails/r servers into scripts/start-r-server and scripts/start-rails-server

--- scripts/start-r-server.sh
+++ scripts/start-r-server.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-# Start the rails server, which hosts the web app.
-rails server &
+
+cd /home/daniel/Desktop/CategoryCompare-WebApp
 
 # Start the R server, which the rails server communicates with.
 # Load the CCWebApp.conf file, which loads libraries used by all connections to the R server.
-R CMD Rserve --RS-source ~/Desktop/CategoryCompare-WebApp/rserve-config/CCWebApp.conf
+R CMD Rserve --RS-source /home/daniel/Desktop/CategoryCompare-WebApp/rserve-config/CCWebApp.conf --no-save
+
+return 0
