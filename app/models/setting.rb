7d8f9c9b451b117ce6c45886a36117d9a85eeef0
Daniel Neel <dneelyep@gmail.com>
2015-12-29 01:12:12 +0000

    Give users the ability to specify the colors of edges displayed in the output graph.

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -2,8 +2,10 @@ class Setting < ActiveRecord::Base
   has_no_table
 
   column :bg_color, :string
+  column :edge_color, :string
 
   def initialize_settings
     self.bg_color = "#FFFFFF"
+    self.edge_color = "#FFFFFF"
   end
 end
