cf9240964b66dab08a25c21df81d5820d4fc33b8
Daniel Neel <dneelyep@gmail.com>
2016-01-02 01:10:36 +0000

    Add schema changes for RServeUtils which were missed in a previous commit.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151227230014) do
+ActiveRecord::Schema.define(version: 20160102004340) do
+
+  create_table "rserve_utils", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "settings", force: true do |t|
     t.string   "bg_color"
