1ccaeaca7f965e712532ebcf2c6e35cca22fb9a8
Daniel Neel <dneelyep@gmail.com>
2016-01-13 22:47:07 +0000

    Extract the GeneList-related code out of the GeneUniverse class, and make a GeneUniverse have a GeneList. Switch GeneUniverse from an ActiveRecord model to an ActiveModel model, since having a table does not make sense.

--- app/models/gene_universe.rb
+++ app/models/gene_universe.rb
@@ -1,40 +1,24 @@
-class GeneUniverse < ActiveRecord::Base
-  has_no_table
-  belongs_to :category_compare
-  column :text_gene_list, :string
-  column :file_gene_list, :string
-  column :category_compare_id, :integer
-
-  # TODO The notion that a model should only have one or the other of a text/file gene list is misleading.
-  #      What I really need is a GeneList class. Then, a model will have a GeneList. And a GeneList can
-  #      have either a file or text source.
-  # TODO This class should probably be renamed to "GeneUniverse" to fit with the app's terminology.
-  # TODO I should be able to say GeneUniverse.FromOrganism("Hs") or similar.
+class GeneUniverse 
+  include ActiveModel::Model
 
   ##
-  # Returns the actual file behind the :file_gene_list field.
-  def file_gene_list_source()
-    self.file_gene_list.tempfile
+  # The actual list of genes that make up this gene universe.
+  attr_accessor :gene_list
+  validates_presence_of :gene_list
+
+  def from_organism(organism_type)
+    # TODO I should be able to say GeneUniverse.FromOrganism("Hs") or similar.
+    raise NotImplementedError("TODO Add this code.")
   end
 
-  def text_gene_list_used?()
-    self.text_gene_list.length > 0
+  def gene_list_attributes=(attributes)
+    @gene_list = GeneList.new(attributes)
   end
 
-  def file_gene_list_used?()
-    !File.zero?(self.file_gene_list_source())
-  end
 
-  ##
-  # Returns a string which represents this gene universe as
-  # a list of integers, which can be interpreted by R.
-  def to_r_gene_list()
-    if text_gene_list_used?()
-      self.text_gene_list.split(' ').map(&:to_i)
-    elsif file_gene_list_used?()
-      File.foreach(self.file_gene_list_source()).map{|line| line.to_i}
-    else
-      raise RuntimeError("Neither a text- or file-based gene list is available. R gene list can't be created.")
+  def initialize(attributes = {})
+    attributes.each do |name, value|
+      send("#{name}=", value)
     end
   end
 end
